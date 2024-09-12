abaa442e112ec84bde910296d23e8e3d9dd5cb8c
RamonM97 <84131829+RamonM97@users.noreply.github.com>
2024-09-12 02:57:55 +0200

    Update ajax-pagination.js

--- userlist/js/ajax-pagination.js
+++ userlist/js/ajax-pagination.js
@@ -1,8 +1,8 @@
 (function ($, Drupal) {
   Drupal.behaviors.ajaxPagination = { //Manejar los clicks en enlaces de paginacion
     attach: function (context, settings) {
-      $(context).find('.pagination-link').on('click', function (e) {
-        e.preventDefault(); //Evitar que el enlace se siga y hacer que se actualice mediante AJAX
+      $('#user-list-wrapper', context).on('click', '.pagination-link', function (e) {
+        e.preventDefault(); //Evita que el enlace siga la URL
 
         var page = $(this).data('page'); //Mediante el atributo data-page recogemos el numero de la pagina
         var url = '/user-list/ajax?page=' + page; //Construimos la URL para la solicitud AJAX
@@ -11,17 +11,17 @@
           url: url,
           type: 'GET',
           success: function (response) {
-            $('#user-list-wrapper').html(response.html); //Reemplazar el contenido con el nuevo HTML
-            //Drupal.attachBehaviors($('#user-list-wrapper')[0], settings);
+            $('#user-list-wrapper').html(response.html);
+            Drupal.attachBehaviors($('#user-list-wrapper')[0], settings); //Vuelve a enlazar el ID
           }
         });
       });
 
-      // Manejar el envío del formulario de búsqueda
+      //Manejar el envio del formulario de busqueda
       $('#user-search-form', context).on('submit', function (e) {
-        e.preventDefault(); //Evitar que el enlace se siga y hacer que se actualice mediante AJAX
+        e.preventDefault();
 
-        var $form = $(this); //Referencia al formulario actual
+        var $form = $(this);
         var url = $form.attr('action'); //URL del formulario para la solicitud AJAX
 
         $.ajax({
@@ -29,8 +29,8 @@
           type: 'POST',
           data: $form.serialize(),
           success: function (response) {
-            $('#user-list-wrapper').html(response); //Reemplazar el contenido con el nuevo HTML
-            //Drupal.attachBehaviors($('#user-list-wrapper')[0], settings);
+            $('#user-list-wrapper').html(response.html); //Reemplazar el contenido con el nuevo HTML
+            Drupal.attachBehaviors($('#user-list-wrapper')[0], settings); //Vuelve a enlazar el ID
           }
         });
       });
